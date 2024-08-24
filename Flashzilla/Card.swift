79fd2584a51183b50a503b579606e8e09594fd75
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-24 09:38:19 +0200

    feat: core app

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Adam Elfsborg on 2024-08-24.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who ?", answer: "Jodie Withttaker")
+}
--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,15 +7,38 @@
 
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 struct ContentView: View {
-    @Environment(\.accessibilityReduceTransparency) var accessibilityReduceTransparency
+    @State private var deck = Array<Card>(repeating: .example, count: 10)
     
     var body: some View {
-        Text("Hello wolrd")
-            .padding()
-            .background(accessibilityReduceTransparency ? .black : .black.opacity(0.5))
-            .foregroundColor(.white)
-            .clipShape(.capsule)
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<deck.count, id: \.self) { index in
+                        CardView(card: deck[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: deck.count)
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        deck.remove(at: index)
     }
 }
 
