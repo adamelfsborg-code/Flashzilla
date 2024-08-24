e572dcf311a06d387ba5c097bd52b641b960f6a1
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-24 08:35:25 +0200

    feat: timer

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    
     var body: some View {
         VStack {
-            Text("Hello")
-            Spacer()
-                .frame(height: 100)
-            
-            Text("World")
+            Text("Hello world")
+                .onReceive(timer, perform: { time in
+                    if counter == 5 {
+                        cancelTimer()
+                    } else {
+                        print("The time is not \(time)")
+                    }
+                    
+                    counter += 1
+                })
         }
-        .contentShape(.rect)
-        .onTapGesture {
-            print("VStack tapped")
-        }
+    }
+    
+    func cancelTimer() {
+        timer.upstream.connect().cancel()
     }
 }
 
