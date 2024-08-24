cf1642e97133fb19c9ab0c0de77f463544e1f548
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-24 07:56:20 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Adam Elfsborg on 2024-08-24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
