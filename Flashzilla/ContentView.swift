35f0c0542f7d9b83b1c3be03e45db6f404fbfbe9
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-24 08:26:06 +0200

    feat: content shape

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -10,21 +10,16 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped")
-                }
+        VStack {
+            Text("Hello")
+            Spacer()
+                .frame(height: 100)
             
-            Circle()
-                .fill(.red)
-                .frame(width: 300, height: 300)
-                .contentShape(.rect)
-                .onTapGesture {
-                    print("Circle tapped")
-                }
+            Text("World")
+        }
+        .contentShape(.rect)
+        .onTapGesture {
+            print("VStack tapped")
         }
     }
 }
