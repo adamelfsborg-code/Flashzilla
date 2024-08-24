a28c8a37806df1809ded1c4eed3753b5531b5006
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-24 08:08:31 +0200

    feat: tap, hold and scale gesture

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount = 0.0
+    @State private var finalAmount = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Double tap")
+            .onTapGesture(count: 2) {
+                print("Double tap")
+            }
+        
+        Text("Long press")
+            .onLongPressGesture {
+                print("Long Press")
+            }
+        Text("2 sek press")
+            .onLongPressGesture(minimumDuration: 2) {
+                print("2 sek press")
+            } onPressingChanged: { inProgress in
+                print("In progress: \(inProgress)")
+            }
+        
+        Text("Scale")
+            .scaleEffect(finalAmount + currentAmount)
+            .gesture(
+                MagnifyGesture()
+                    .onChanged { value in
+                        currentAmount = value.magnification
+                    }
+                    .onEnded { value in
+                        finalAmount += currentAmount
+                        currentAmount = 0
+                    }
+            )
     }
 }
 
