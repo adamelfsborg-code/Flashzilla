5ac713d2f717d7c1b70fa33042536659bfec181f
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-25 09:35:41 +0200

    feat: timer

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -15,7 +15,15 @@ extension View {
 }
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @Environment(\.scenePhase) var scenePhase
+    
     @State private var deck = Array<Card>(repeating: .example, count: 10)
+    @State private var timeRemaning = 100
+    @State private var isActive = true
+    
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
@@ -23,6 +31,14 @@ struct ContentView: View {
                 .resizable()
                 .ignoresSafeArea()
             VStack {
+                Text("Time: \(timeRemaning)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                
                 ZStack {
                     ForEach(0..<deck.count, id: \.self) { index in
                         CardView(card: deck[index]) {
@@ -33,12 +49,62 @@ struct ContentView: View {
                         .stacked(at: index, in: deck.count)
                     }
                 }
+                .allowsHitTesting(timeRemaning > 0)
+                
+                if deck.isEmpty {
+                    Button("Start again", action: resetDeck)
+                        .padding()
+                        .background(.white)
+                        .foregroundColor(.black)
+                        .clipShape(.capsule)
+                }
             }
+            if accessibilityDifferentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            guard timeRemaning > 0 else { return }
+            
+            timeRemaning -= 1
+        }
+        .onChange(of: scenePhase) {
+            guard deck.isEmpty == false else { return }
+            isActive = scenePhase == .active
         }
     }
     
     func removeCard(at index: Int) {
         deck.remove(at: index)
+        
+        isActive = deck.isEmpty == false
+    }
+    
+    func resetDeck() {
+        deck = Array<Card>(repeating: .example, count: 10)
+        timeRemaning = 100
+        isActive = true
     }
 }
 
